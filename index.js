0b4a990f8871af0942aef3ced042cc8c12f30b6a
XieZongChen <952937570@qq.com>
2024-01-06 18:55:19 +0800

    docs: fix readme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * 引入编译后的文件
- * 通常 wasm-bindgen 会在 Cargo.toml 所在目录生成一个 pkg 文件夹，里面包含了编译后的文件
+ * 通常 wasm-bindgen 会在 Cargo.toml 所在目录生成一个 pkg 目录，里面包含了编译后的文件
  * 所要引入的文件名随 Cargo.toml 中的 name 配置项而定
  */
 const rust = import('./pkg/wasm_rust.js');
